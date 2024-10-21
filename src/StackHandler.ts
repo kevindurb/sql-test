d5ab6b78cad8f8c7972dbf2154a1fc0986c280b2
Kevin Durbin <kevindurb@fastmail.com>
2024-10-21 10:14:33 -0600

    Build wip StackHandler

---
+++ src/StackHandler.ts
@@ -0,0 +1,29 @@
+type UnknownFunc = () => unknown | Promise<unknown>;
+
+type Handler<ValueType, ResultType> = (
+  value: ValueType,
+  cb: UnknownFunc | undefined,
+) => ResultType | Promise<ResultType>;
+
+export class StackHandler<ValueType, ResultType> {
+  private handlers: Handler<ValueType, ResultType>[] = [];
+
+  push(handler: Handler<ValueType, ResultType>) {
+    this.handlers.push(handler);
+  }
+
+  private enqueueNext(value: ValueType, idx: number): UnknownFunc | undefined {
+    const next = this.handlers[idx];
+    if (!next) return;
+
+    return async () => {
+      return await next(value, this.enqueueNext(value, idx + 1));
+    };
+  }
+
+  async handle(value: ValueType) {
+    const next = this.enqueueNext(value, 0);
+    if (next) return await next();
+    return undefined;
+  }
+}
